b6679ba07e4764dfbafbfb2d59c48a47b97b5808
segangoh <chrisryn@naver.com>
2024-05-13 14:26:31 +0900

    Update stepback() in js, delete nett button, update css js link

--- web/app/static/js/transfer.js
+++ web/app/static/js/transfer.js
@@ -11,76 +11,14 @@ nextButton.addEventListener('click', ()=>{
     currentStep = currentStep + 1;
 });
 
-function step1Back(){
-    let step1 = 0;
-
+function stepBack(step){
     stepTargets[currentStep].classList.remove('active');
     currentStep --;
 
-    while(currentStep > step1){
+    while(currentStep > step){
         stepTargets[currentStep].classList.remove('prev');
         currentStep --;
     }
     stepTargets[currentStep].classList.remove('prev');
     stepTargets[currentStep].classList.add('active');
-}
-
-function step2Back(){
-    let step2 = 1;
-
-    stepTargets[currentStep].classList.remove('active');
-    currentStep --;
-
-    while(currentStep > step2){
-        stepTargets[currentStep].classList.remove('prev');
-        currentStep --;
-    }
-    stepTargets[currentStep].classList.remove('prev');
-    stepTargets[currentStep].classList.add('active');
-
-}
-
-function step3Back(){
-    let step3 = 2;
-
-    stepTargets[currentStep].classList.remove('active');
-    currentStep --;
-
-    while(currentStep > step3){
-        stepTargets[currentStep].classList.remove('prev');
-        currentStep --;
-    }
-    stepTargets[currentStep].classList.remove('prev');
-    stepTargets[currentStep].classList.add('active');
-
-}
-
-function step4Back(){
-    let step1 = 3;
-
-    stepTargets[currentStep].classList.remove('active');
-    currentStep --;
-
-    while(currentStep > step4){
-        stepTargets[currentStep].classList.remove('prev');
-        currentStep --;
-    }
-    stepTargets[currentStep].classList.remove('prev');
-    stepTargets[currentStep].classList.add('active');
-
-}
-
-function step5Back(){
-    let step5 = 4;
-
-    stepTargets[currentStep].classList.remove('active');
-    currentStep --;
-
-    while(currentStep > step5){
-        stepTargets[currentStep].classList.remove('prev');
-        currentStep --;
-    }
-    stepTargets[currentStep].classList.remove('prev');
-    stepTargets[currentStep].classList.add('active');
-
 }
