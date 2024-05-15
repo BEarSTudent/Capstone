c76ac5be389d2c00c571b861960e1579196d40ea
JuYeolYang <juyeolyang212@gmail.com>
2024-05-16 03:01:32 +0900

    everyting related to the result page

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, url_for, request, session, current_app, redirect
+from flask import Flask, render_template, url_for, request, session, current_app, redirect, send_file
+import json
+from PIL import Image
+from io import BytesIO
+import base64
+import os
 app = Flask(__name__)
+current_path = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def hello():
@@ -12,6 +18,28 @@ def transfer_page():
 @app.route('/transfer/wait')
 def wait():
     return render_template("/transfer/wait.html")
+
+@app.route('/transfer/result', methods=["GET", "POST"])
+def result():
+    image_path = "images/sample_image.png"
+    if request.method == "POST":
+        json_data = request.get_json()
+        dict_data = json.loads(json.dumps(json_data))
+        
+        image_name = dict_data['name']
+        image_path = "images/transfer/" + str(image_name)
+
+        image = dict_data['img']
+        image = Image.open(BytesIO(base64.b64encode(image)))
+        image.save(image_path)
+        
+    return render_template('/transfer/result.html', image = image_path)
+
+@app.route('/transfer/download/<path:filename>')
+def download(filename):
+    filename = "static/" + filename
+    return send_file(filename,
+                     as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
