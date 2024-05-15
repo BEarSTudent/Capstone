7b0d0c610116869856a3db7fd74141a36c744076
JuYeolYang <juyeolyang212@gmail.com>
2024-05-15 11:06:11 +0900

    add wait page

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, request, session, current_app, redirect
+import requests
+import json
+import base64
+import cv2
+import random
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
 
-@app.route('/transfer/waiting')
+@app.route('/transfer')
+def transfer_page():
+    return render_template("/transfer/transfer.html")    
+
+@app.route('/transfer/wait')
 def wait():
-    return render_template('/transfer/wait.html')
+    return render_template("/transfer/wait.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
